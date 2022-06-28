4ec73eb0d522a955a6e2eed6e93fae37071d2eae
Sasidhar Chintapalli <codersasi@gmail.com>
2022-06-28 12:59:53 +0530

    add day30 end and change password manager with search and error handling

--- day29/passwd_manager/main.py
+++ day29/passwd_manager/main.py
@@ -2,6 +2,8 @@ import random
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -24,27 +26,65 @@ def generate_password():
     passwd_entry.insert(0, password)
     pyperclip.copy(text=password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_entry.get()
     username = username_entry.get()
     password = passwd_entry.get()
+    new_data = {
+        website: {
+            "email": username,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Website/Password cannot be empty")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered\n"
-                                               f"Email: {username}\n"
-                                               f"Password: {password}\n"
-                                               f"Is it ok to save?")
-        if is_ok:
-            with open("data.txt", 'a') as file:
-                file.write(f"{website} | {username} | {password}\n")
-                website_entry.delete(0, END)
-                username_entry.delete(0, END)
-                username_entry.insert(0, "sasi.ch85@gmail.com")
-                passwd_entry.delete(0, END)
+        # is_ok = messagebox.askokcancel(title=website,
+        #                                message=f"These are the details entered\n"
+        #                                        f"Email: {username}\n"
+        #                                        f"Password: {password}\n"
+        #                                        f"Is it ok to save?")
+        # if is_ok:
+        try:
+            with open("data.json", 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            username_entry.delete(0, END)
+            username_entry.insert(0, "abc@gmail.com")
+            passwd_entry.delete(0, END)
+
+
+def search_password():
+    website = website_entry.get()
+    password_data = {}
+    data = {}
+    try:
+        if len(website) != 0:
+            with open("data.json", 'r') as data_file:
+                data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            password_data = data.get(website)
+            email = password_data.get('email')
+            password = password_data.get('password')
+            messagebox.showinfo(title=website,
+                                message=f"Email: {email}\n"
+                                        f"Password: {password}\n")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,20 +107,22 @@ passwd_label.grid(row=3, column=0)
 
 # Entries
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, columnspan=1)
 
 username_entry = Entry(width=35)
-username_entry.insert(0, "sasi.ch85@gmail.com")
+username_entry.insert(0, "abc@gmail.com")
 username_entry.grid(row=2, column=1, columnspan=2)
 passwd_entry = Entry(width=21)
-passwd_entry.grid(row=3, column=1)
+passwd_entry.grid(row=3, column=1, columnspan=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", command=search_password, width=15)
+search_button.grid(row=1, column=2, columnspan=1)
 
 window.mainloop()
